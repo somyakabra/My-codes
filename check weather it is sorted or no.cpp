70149585eda0c53ef6ab577f2cb3ec7e769b8229
Somya Kabra <97023859+somyakabra@users.noreply.github.com>
2023-05-31 10:57:46 +0530

    Create check whether it is sorted or no.cpp

---
+++ check weather it is sorted or no.cpp
@@ -0,0 +1,27 @@
+// naive solution time complexcity O(N^2)
+
+bool isSorted(int arr[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = i + 1; j < n; j++)
+		{
+			if(arr[j] < arr[i])
+				return false;
+		}
+	}
+
+	return true;
+} 
+//Efficient method or optimisted solution
+//time complexcity O(N)
+bool isSorted(int arr[], int n)
+{
+	for(int i = 1; i < n; i++)
+	{
+	    if(arr[i] < arr[i - 1])
+	        return false;
+	}
+
+	return true;
+} 
